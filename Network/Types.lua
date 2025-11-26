75987d74d1e8e96a45c5bee8dc151e4d2c8a8822
vincordian <vincar100@icloud.com>
2025-11-26 10:30:06 -0600

    Update Types.lua

--- Network/Types.lua
+++ Network/Types.lua
@@ -1,16 +1,20 @@
 export type NetworkInfo = {
 	NetworkingDirection: "ClientToServer"|"ServerToClient"|"any", --Great if you don't want any accidents to happen
-	
+
 	Name: string, --Name of the remote
-	Place: Instance, --Place the remote will be stored
-	
+
 	ServerFunction: (any) -> (any),
 	ClientFunction: (any) -> (any),
-	
+
 	ReturnToServer: (any) -> (any), --Fires when server fires the remote, the client fires to server back with data
 	ReturnToClient: (any) -> (any), --Fires when client fires the remote, the server fires to client back with data
-	
+
 	Target: {Player}|nil --Will fire all clients if nil
+}
+
+export type Threads = {
+	Server: {() -> ()},
+	Client: {() -> ()}
 }
 
 return {}
