c89c6559681427650100cb77daf4acaed3f2e278
vincordian <vincar100@icloud.com>
2025-11-28 10:38:43 -0600

    Update Types.lua

--- Network/Types.lua
+++ Network/Types.lua
@@ -1,18 +1,21 @@
 export type NetworkInfo = {
 	NetworkingDirection: "ClientToServer"|"ServerToClient"|"any", --Great if you don't want any accidents to happen
-	
+
 	Name: string, --Name of the remote
-	
+
 	ServerFunction: (any) -> (nil), --Fires when OnServerEvent ends
 	ClientFunction: (any) -> (nil), --Fires when OnClientEvent ends
-	
+
+	ServerFunctionCalledOnReturn: boolean, --Controls if the server function gets called on return
+	ClientFunctionCalledOnReturn: boolean, --Controls if the client function gets called on return
+
 	ReturnToServer: (any) -> (any), --Fires when server fires the remote, the client fires to server back with data
 	ReturnToClient: (any) -> (any), --Fires when client fires the remote, the server fires to client back with data
-	
+
 	AutoAddPlayers: boolean, --Will automatically add players left out to the target table if true
-	
+
 	Target: {Player}|nil, --Will fire all clients if nil
-	
+
 	Threads: {
 		Server: {(any) -> (any)},--Secondary server functions basically
 		Client: {(any) -> (any)}, --Secondary client functions basically
