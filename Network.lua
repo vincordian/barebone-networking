fedd7bea946bbf8c6f7197cea3dadf4eb38a06a0
vincordian <vincar100@icloud.com>
2025-11-28 11:53:28 -0600

    Update Network.lua

--- Network.lua
+++ Network.lua
@@ -5,6 +5,8 @@ Client shouldn't create any server functions, only the server should do that & v
 Server & client share separate modules, and only connect by a remote.
 For example, you create Network.new {Name = "Test", ClientFunction = function() end}, the client function will only work if you're on the client as they share different metatables.
 I was gonna let it do the opposite, letting the server give the client to run functions, but that's already possible with the returns
+
+
 ]]
 
 --Services--
@@ -50,19 +52,19 @@ function Network.new(NetworkInfo: Types.NetworkInfo)
 
 	if RunService:IsServer() then assert(not NetworkInfo.ClientFunction, "You're trying to create a client function while on the server. What you're trying to do is stupid.") assert(not NetworkInfo.ReturnToServer, "You're trying to create a server return while on the server. What you're trying to do is stupid.") end
 	if RunService:IsClient() then assert(not NetworkInfo.ServerFunction, "You're trying to create a server function while on the client. What you're trying to do is stupid.") assert(not NetworkInfo.ReturnToClient, "You're trying to create a client return while on the client. What you're trying to do is stupid.") end
-	
+
 	NetworkInfo.NetworkingDirection = NetworkInfo.NetworkingDirection or "any"
-	
+
 	NetworkInfo.Target = if RunService:IsClient() then {} else NetworkInfo.Target or Players:GetPlayers()
-	
+
 	NetworkInfo.ServerFunction = NetworkInfo.ServerFunction or function() end
 	NetworkInfo.ClientFunction = NetworkInfo.ClientFunction or function() end
-	
+
 	NetworkInfo.ReturnToServer = NetworkInfo.ReturnToServer or function(...) return ... end
 	NetworkInfo.ReturnToClient = NetworkInfo.ReturnToClient or function(...) return ... end
-	
+
 	NetworkInfo.AutoAddPlayers = if RunService:IsClient() then false else NetworkInfo.AutoAddPlayers or false
-	
+
 	NetworkInfo.ServerFunctionCalledOnReturn = NetworkInfo.ServerFunctionCalledOnReturn or false
 	NetworkInfo.ClientFunctionCalledOnReturn = NetworkInfo.ClientFunctionCalledOnReturn
 
@@ -73,31 +75,31 @@ function Network.new(NetworkInfo: Types.NetworkInfo)
 			NetworkInfo.Threads[ThreadType] = {}
 		end
 	end
-	
+
 	if self.AutoAddPlayers then
 		self.AutoAddPlayersConnection = Players.PlayerAdded:Connect(function(Player)
 			table.insert(self.Target, Player)
 		end)
 	end
-	
+
 	self.Name = NetworkInfo.Name
 	self.NetworkingDirection = NetworkInfo.NetworkingDirection
-	
+
 	self.Target = NetworkInfo.Target
 
 	self.ServerFunction = NetworkInfo.ServerFunction
 	self.ClientFunction = NetworkInfo.ClientFunction
-	
+
 	self.ReturnToServer = NetworkInfo.ReturnToServer
 	self.ReturnToClient = NetworkInfo.ReturnToClient
-	
+
 	self.AutoAddPlayers = NetworkInfo.AutoAddPlayers
-	
+
 	self.ServerFunctionCalledOnReturn = NetworkInfo.ServerFunctionCalledOnReturn
 	self.ClientFunctionCalledOnReturn = NetworkInfo.ClientFunctionCalledOnReturn
-	
+
 	self.Threads = NetworkInfo.Threads
-	
+
 	if self.AutoAddPlayers then
 		local AutoAddPlayersConnection = Players.PlayerAdded:Connect(function(Player)
 			table.insert(self.Target, Player)
