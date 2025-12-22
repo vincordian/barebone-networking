40fd30be397b0fd84f162812c8a55e47954d214d
vincordian <vincar100@icloud.com>
2025-12-21 22:24:34 -0600

    More type check + bugfixes

--- Network.lua
+++ Network.lua
@@ -1,6 +1,7 @@
 --[[Notes:
 
 client & server can only create their respective functions
+it'll error if you have a client network but not a server one
 
 ]]
 
@@ -40,7 +41,9 @@ export type NetworkInfo = {
 		ClientReturnThreads: {(any) -> (any)}, --Fires when server returns data
 	},
 
-	AnticheatFunction: (any) -> (boolean)
+	AnticheatFunction: (any) -> (boolean),
+	
+	KickOnAnticheatTrigger:boolean
 }
 
 
@@ -91,62 +94,66 @@ local THREADS_TEMPLATE = {
 
 ]=]
 function Network.new(NetworkInfo: NetworkInfo)
-
+	
 	assert(NetworkInfo.Name, "No name provided for the network")
-
+	
 	--Important to do this first, as if done in the later IsServer & isClient and throws an error, will waste memory
-
+	
 	if RunService:IsServer() then
 		assert(not NetworkInfo.ClientFunction, "You're trying to create a client function while on the server. What you're trying to do is stupid.")
 	end
-
+	
 	if RunService:IsClient() then
 		assert(not NetworkInfo.ServerFunction, "You're trying to create a server function while on the client. What you're trying to do is stupid.")
 		assert(not NetworkInfo.ReturnToClient, "You're trying to create a client return while on the client. What you're trying to do is stupid.")
 	end
-
-
+	
+	
 	local self = setmetatable({}, Network)
-
+	
 	self.Name = NetworkInfo.Name
 	self.NetworkingDirection = NetworkInfo.NetworkingDirection or "any"
-
+	
 	self.Target = if RunService:IsClient() then {} else NetworkInfo.Target or Players:GetPlayers()
-
+	
 	self.ServerFunction = NetworkInfo.ServerFunction or function() end
 	self.ClientFunction = NetworkInfo.ClientFunction or function() end
-
+	
 	self.ClientFunctionCalledOnReturn = NetworkInfo.ClientFunctionCalledOnReturn or false
-
+	
 	self.ReturnToClient = if NetworkInfo.ReturnToClient then NetworkInfo.ReturnToClient elseif NetworkInfo.ReturnToClient == true then function(Player, ...) return ... end else NetworkInfo.ReturnToClient
-
+	
 	self.AutoAddPlayers = if RunService:IsClient() then false else NetworkInfo.AutoAddPlayers or false
-
-
+	
 	self.Threads = NetworkInfo.Threads or table.clone(THREADS_TEMPLATE)
-
+	
+	self.KickOnAnticheatTrigger = NetworkInfo.KickOnAnticheatTrigger or false
+	
 	for ThreadType, _ in THREADS_TEMPLATE do
 		if not self.Threads[ThreadType] then
 			self.Threads[ThreadType] = {}
 		end
 	end
-
+	
+	
 	self.AnticheatFunction = NetworkInfo.AnticheatFunction or function() return true end
-
+	
+	
 	if self.AutoAddPlayers then
 		self.AutoAddPlayersConnection = Players.PlayerAdded:Connect(function(Player)
 			table.insert(self.Target, Player)
 		end)
 	end
-
-
+	
+	
+	
 	--a really good way to remove local variables if you don't want them
 	do
 		local Folder = ReplicatedStorage:FindFirstChild("NetworkRemotes")
-
+		
 		if not Folder then
 			if RunService:IsClient() then
-				Folder = ReplicatedStorage:WaitForChild("NetworkRemotes", 5)
+				Folder = ReplicatedStorage:WaitForChild("NetworkRemotes", 10)
 				assert(Folder, "No folder for network found.")
 			else
 				Folder = Instance.new("Folder")
@@ -154,13 +161,13 @@ function Network.new(NetworkInfo: NetworkInfo)
 				Folder.Parent = ReplicatedStorage
 			end
 		end
-
+		
 		self.Remote = Folder:FindFirstChild(self.Name)
-
-
+		
+		
 		if not self.Remote then
 			if RunService:IsClient() then
-				self.Remote = Folder:WaitForChild(self.Name, 5)
+				self.Remote = Folder:WaitForChild(self.Name, 10)
 				assert(self.Remote, "No remote for network found.")
 			else
 				self.Remote = Instance.new("RemoteEvent")
@@ -168,14 +175,23 @@ function Network.new(NetworkInfo: NetworkInfo)
 				self.Remote.Parent = Folder
 			end
 		end
-
-
+		
+		
 		if RunService:IsServer() then
-			self.Remote.OnServerEvent:Connect(function(Player, ...)
-
-				assert(self.AnticheatFunction(Player, ...), `Anticheat triggered by {Player}`)
-
+			self.Remote.OnServerEvent:Connect(function(Player:Player, ...)
+				
+				if self.AnticheatFunction(Player, ...) then
+					
+					warn(`Anticheat triggered by {Player}`)
+					if self.KickOnAnticheatTrigger then pcall(function() Player:Kick() end) end
+					
+					return
+				end
+				
+				
 				self.ServerFunction(Player, ...)
+				
+				
 				if self.ReturnToClient then
 					if type(self.ReturnToClient) == "function" then
 						self.Remote:FireClient(Player, self.ReturnToClient(Player, ...), "__return")
@@ -223,7 +239,7 @@ function Network:FireClient(...)
 	assert(RunService:IsServer(), "You're trying to fire clients while on the client. What you're trying to do is stupid.")
 	assert(self.NetworkingDirection == "ServerToClient" or self.NetworkingDirection == "any", "You're trying to fire the client while having the networking direction of ClientToServer.")
 
-	for _, Player in self.Target do
+	for _, Player:Player in self.Target do
 		self.Remote:FireClient(Player, ...)
 	end
 
@@ -245,6 +261,7 @@ end
 --Ends the network, should be used on the server
 function Network:End()
 	assert(RunService:IsServer(), "You're trying to end the network while on the client. What you're trying to do is stupid.")
+	self.Remote:Destroy()
 	setmetatable(self, nil)
 	table.clear(self)
 	self = nil
